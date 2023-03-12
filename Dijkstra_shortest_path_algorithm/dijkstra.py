976497881e70e52e193cdfb63a24b54d5663de31
tarunreddyy <tarunsaireddy1998@gmail.com>
2023-03-12 18:55:03 -0400

    Optimized algorithm

--- Dijkstra_shortest_path_algorithm/dijkstra.py
+++ Dijkstra_shortest_path_algorithm/dijkstra.py
@@ -7,73 +7,74 @@ class Dijkstra:
         self.COLS = COLS
         self.MAP = MAP
 
-    def ActionMoveLeft(self, current_node):
-        row, col = current_node
-        if col > 0:
-            return (row, col-1)
+    def ActionMoveLeft(self, curr_node):
+        r, c = curr_node
+        if c > 0:
+            return (r, c-1)
         return None
 
-    def ActionMoveRight(self, current_node):
-        row, col = current_node
-        if col < self.COLS-1:
-            return (row, col+1)
+    def ActionMoveRight(self, curr_node):
+        r, c = curr_node
+        if c < self.COLS-1:
+            return (r, c+1)
         return None
 
-    def ActionMoveUp(self, current_node):
-        row, col = current_node
-        if row > 0:
-            return (row-1, col)
+    def ActionMoveUp(self, curr_node):
+        r, c = curr_node
+        if r > 0:
+            return (r-1, c)
         return None
 
-    def ActionMoveDown(self, current_node):
-        row, col = current_node
-        if row < self.ROWS-1:
-            return (row+1, col)
+    def ActionMoveDown(self, curr_node):
+        r, c = curr_node
+        if r < self.ROWS-1:
+            return (r+1, c)
         return None
 
-    def ActionMoveUpLeft(self, current_node):
-        row, col = current_node
-        if row > 0 and col > 0:
-            return (row-1, col-1)
+    def ActionMoveUpLeft(self, curr_node):
+        r, c = curr_node
+        if r > 0 and c > 0:
+            return (r-1, c-1)
         return None
 
-    def ActionMoveUpRight(self, current_node):
-        row, col = current_node
-        if row > 0 and col < self.COLS-1:
-            return (row-1, col+1)
+    def ActionMoveUpRight(self, curr_node):
+        r, c = curr_node
+        if r > 0 and c < self.COLS-1:
+            return (r-1, c+1)
         return None
 
-    def ActionMoveDownLeft(self, current_node):
-        row, col = current_node
-        if row < self.ROWS-1 and col > 0:
-            return (row+1, col-1)
+    def ActionMoveDownLeft(self, curr_node):
+        r, c = curr_node
+
+        if r < self.ROWS-1 and c > 0:
+            return (r+1, c-1)
         return None
 
-    def ActionMoveDownRight(self, current_node):
-        row, col = current_node
-        if row < self.ROWS-1 and col < self.COLS-1:
-            return (row+1, col+1)
+    def ActionMoveDownRight(self, curr_node):
+        r, c = curr_node
+        if r < self.ROWS-1 and c < self.COLS-1:
+            return (r+1, c+1)
         return None
 
-    def heuristic(self, node1, node2):
+    def cal_cost(self, node1, node2):
         # Calculate the heuristic cost between two nodes using Manhattan distance
         return abs(node1[0] - node2[0]) + abs(node1[1] - node2[1])
 
-    def get_neighbors(self, current_node):
+    def get_neighbors(self, curr_node):
         # Define all possible actions
         actions = [self.ActionMoveLeft, self.ActionMoveRight, self.ActionMoveUp, self.ActionMoveDown,
                    self.ActionMoveUpLeft, self.ActionMoveUpRight, self.ActionMoveDownLeft, self.ActionMoveDownRight]
 
         # Initialize empty list of neighbors
-        neighbors = []
+        all_neighbors = []
 
         # Loop through all possible actions and add valid neighbors to the list
         for action in actions:
-            neighbor = action(current_node)
-            if neighbor is not None and neighbor[0] >= 0 and neighbor[0] < self.MAP.shape[0] and neighbor[1] >= 0 and neighbor[1] < self.MAP.shape[1] and self.MAP[neighbor[0]][neighbor[1]] != -1:
-                neighbors.append(neighbor)
+            n = action(curr_node)
+            if n is not None and n[0] >= 0 and n[0] < self.MAP.shape[0] and n[1] >= 0 and n[1] < self.MAP.shape[1] and self.MAP[n[0]][n[1]] != -1:
+                all_neighbors.append(n)
 
-        return neighbors
+        return all_neighbors
 
 
     def generate_path(self, parents, final_node):
@@ -89,17 +90,17 @@ class Dijkstra:
 
     def explore(self, start_node, end_node):
         # Initialize the priority queue with the starting node
-        pq = [(0, start_node)]
+        queue = [(0, start_node)]
         # Initialize the explored dictionary with the starting node and its cost
         explored = {start_node: 0}
         # Initialize the parents dictionary to keep track of the parent of each explored node
         parents = {}
-
+        # Initialize list for all explored node
         explored_nodes = []
         # Loop until the priority queue is empty
-        while pq:
+        while queue:
             # Get the node with the lowest cost from the priority queue
-            curr_cost, curr_node = heapq.heappop(pq)
+            curr_cost, curr_node = heapq.heappop(queue)
 
             # If we have reached the end node, generate the path and return it
             if curr_node == end_node:
@@ -110,22 +111,22 @@ class Dijkstra:
             neighbors = self.get_neighbors(curr_node)
 
             # Loop through each neighbor of the current node
-            for neighbor in neighbors:
+            for n in neighbors:
                 # Calculate the cost to move to the neighbor node
-                if abs(curr_node[0] - neighbor[0]) == 1 and abs(curr_node[1] - neighbor[1]) == 1:
-                    move_cost = 1.4
+                if abs(curr_node[0] - n[0]) == 1 and abs(curr_node[1] - n[1]) == 1:
+                    cost = 1.4
                 else:
-                    move_cost = 1.0
+                    cost = 1.0
                 # Calculate the total cost to move to the neighbor node
-                neighbor_cost = curr_cost + move_cost
+                n_cost = curr_cost + cost
 
                 # If the neighbor node has not been explored or the cost to reach it is less than the previously explored cost, update the explored dictionary and priority queue
-                if neighbor not in explored or neighbor_cost < explored[neighbor]:
-                    explored[neighbor] = neighbor_cost
-                    priority = neighbor_cost + self.heuristic(neighbor, end_node)
-                    heapq.heappush(pq, (priority, neighbor))
-                    parents[neighbor] = curr_node
-                    explored_nodes.append(neighbor)
+                if n not in explored or n_cost < explored[n]:
+                    explored[n] = n_cost
+                    priority = n_cost + self.cal_cost(n, end_node)
+                    heapq.heappush(queue, (priority, n))
+                    parents[n] = curr_node
+                    explored_nodes.append(n)
 
         # If the priority queue is empty and we have not found the end node, return None
         Tk().wm_withdraw()
